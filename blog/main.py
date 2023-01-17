ee82c455a2b148d506fb3d14433df24f0fec491e
ighorstepanenko <ighor_stepanenko@list.ru>
2023-01-17 17:54:21 +0300

    Fourth commit

--- blog/main.py
+++ blog/main.py
@@ -47,72 +47,93 @@ def update_menu(target_menu_id, request: schemas.Menus, db: Session = Depends(ge
     menu = db.query(models.Menus).filter(models.Menus.id == target_menu_id)
     if not menu:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'menu not found')
+    menu.update(request)
+    db.commit()
+
+
+@app.patch('/api/v1/menus/{target_menu_id}', status_code=status.HTTP_200_OK, tags=['Menus'])
+def update_menu(target_menu_id, request: schemas.Menus, db: Session = Depends(get_db)):
+    menu = db.query(models.Menus).filter(models.Menus.id == target_menu_id).first()
+    if not menu:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'menu not found')
+    menu.title = request.title
+    menu.description = request.description
     db.add(menu)
     db.commit()
     db.refresh(menu)
-    return  {
-            "id": menu.id,
-            "title": menu.title,
-            "description": menu.description,
-            "submenus_count": menu.submenus_count,
-            "dishes_count": menu.dishes_count
-        }
+    return menu
 
 
-@app.delete('/api/v1/menus/{target_menu_id}', status_code=status.HTTP_204_NO_CONTENT,
+@app.delete('/api/v1/menus/{target_menu_id}', status_code=status.HTTP_200_OK,
             tags=['Menus'])
-def deleate_menu(menu_id, db: Session = Depends(get_db)):
-    menu = db.query(models.Menus).filter(models.Menus.id == menu_id)
-    if not menu.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    menu.delete()
+def deleate_menu(target_menu_id, db: Session = Depends(get_db)):
+    db.query(models.Menus).filter(models.Menus.id == target_menu_id).delete(synchronize_session=False)
     db.commit()
 
 
-@app.get('/api/v1/menus{target_menu_id}/submenus', status_code=status.HTTP_200_OK,
-         tags=['Submenus'])
-def all_submenus(db: Session = Depends(get_db)):
-    submenus = db.query(models.Submenus).all()
-    return submenus
-
-
 @app.post('/api/v1/menus/{target_menu_id}/submenus', status_code=status.HTTP_201_CREATED, tags=['Submenus'])
-def create_submenu(request: schemas.Submenus, db: Session = Depends(get_db)):
-    new_submenu = models.Submenus(id=request.id, title=request.title, description=request.description,
-                                  dishes_count=request.dishes_count)
+def create_menu(request: schemas.Submenus, db: Session = Depends(get_db)):
+    new_submenu = models.Submenus(title=request.title, description=request.description)
     db.add(new_submenu)
     db.commit()
     db.refresh(new_submenu)
     return new_submenu
 
 
+@app.get('/api/v1/menus/{target_menu_id}/submenus', status_code=status.HTTP_200_OK, tags=['Submenus'])
+def all_submenus(db: Session = Depends(get_db)):
+    submenus = db.query(models.Submenus).all()
+    return submenus if submenus else []
+
+
 @app.get('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}', status_code=status.HTTP_200_OK,
          tags=['Submenus'])
-def get_submenu(submenu_id, db: Session = Depends(get_db)):
-    submenu = db.query(models.Submenus).filter(models.Submenus.id == submenu_id).first()
+def get_submenu(target_submenu_id, db: Session = Depends(get_db)):
+    submenu = db.query(models.Submenus).filter(models.Submenus.id == target_submenu_id).first()
     if not submenu:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'submenu not found')
     return submenu
 
 
 @app.put('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}', status_code=status.HTTP_202_ACCEPTED,
-         tags=['Submenus'])
-def update_submenu(submenu_id, request: schemas.Submenus, db: Session = Depends(get_db)):
-    submenu = db.query(models.Submenus).filter(models.Submenus.id == submenu_id)
-    if not submenu.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+         response_model=schemas.ShowSubmenu, tags=['Submenus'])
+def update_submenu(target_submenu_id, request: schemas.Submenus, db: Session = Depends(get_db)):
+    submenu = db.query(models.Submenus).filter(models.Submenus.id == target_submenu_id)
+    if not submenu:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'submenu not found')
     submenu.update(request)
     db.commit()
 
 
-@app.delete('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}', status_code=status.HTTP_204_NO_CONTENT,
-            tags=['Submenus'])
-def deleate_submenu(submenu_id, db: Session = Depends(get_db)):
-    submenu = db.query(models.Submenus).filter(models.Submenus.id == submenu_id)
-    if not submenu.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    submenu.delete()
+@app.patch('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}', status_code=status.HTTP_200_OK,
+           tags=['Submenus'])
+def update_submenu(target_submenu_id, request: schemas.Submenus, db: Session = Depends(get_db)):
+    submenu = db.query(models.Submenus).filter(models.Submenus.id == target_submenu_id).first()
+    if not submenu:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'submenu not found')
+    submenu.title = request.title
+    submenu.description = request.description
+    db.add(submenu)
     db.commit()
+    db.refresh(submenu)
+    return submenu
+
+
+@app.delete('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}', status_code=status.HTTP_200_OK,
+            tags=['Submenus'])
+def deleate_submenu(target_submenu_id, db: Session = Depends(get_db)):
+    db.query(models.Submenus).filter(models.Submenus.id == target_submenu_id).delete(synchronize_session=False)
+    db.commit()
+
+
+@app.post('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes', status_code=status.HTTP_201_CREATED,
+          tags=['Dishes'])
+def create_dish(request: schemas.Dishes, db: Session = Depends(get_db)):
+    new_dish = models.Dishes(title=request.title, description=request.description)
+    db.add(new_dish)
+    db.commit()
+    db.refresh(new_dish)
+    return new_dish
 
 
 @app.get('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes', status_code=status.HTTP_200_OK,
@@ -122,40 +143,42 @@ def all_dishes(db: Session = Depends(get_db)):
     return dishes
 
 
-@app.post('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes', status_code=status.HTTP_201_CREATED,
-          tags=['Dishes'])
-def create_dish(request: schemas.Dishes, db: Session = Depends(get_db)):
-    new_dishes = models.Dishes(id=request.id, title=request.title, description=request.description, price=request.price)
-    db.add(new_dishes)
-    db.commit()
-    db.refresh(new_dishes)
-    return new_dishes
-
-
 @app.get('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes/{target_dish_id}',
          status_code=status.HTTP_200_OK, tags=['Dishes'])
-def get_dish(dish_id, db: Session = Depends(get_db)):
-    dish = db.query(models.Dishes).filter(models.Dishes.id == dish_id).first()
+def get_dish(target_dish_id, db: Session = Depends(get_db)):
+    dish = db.query(models.Dishes).filter(models.Dishes.id == target_dish_id).first()
     if not dish:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'dish not found')
     return dish
 
 
 @app.put('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes/{target_dish_id}',
-         status_code=status.HTTP_202_ACCEPTED, tags=['Dishes'])
-def update_dish(dish_id, request: schemas.Dishes, db: Session = Depends(get_db)):
-    dish = db.query(models.Dishes).filter(models.Dishes.id == dish_id)
-    if not dish.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+         status_code=status.HTTP_202_ACCEPTED, response_model=schemas.ShowDish,
+         tags=['Dishes'])
+def update_dish(target_dish_id, request: schemas.Dishes, db: Session = Depends(get_db)):
+    dish = db.query(models.Dishes).filter(models.Dishes.id == target_dish_id)
+    if not dish:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'dish not found')
     dish.update(request)
     db.commit()
 
 
+@app.patch('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes/{target_dish_id}',
+           status_code=status.HTTP_200_OK, tags=['Dishes'])
+def update_dish(target_dish_id, request: schemas.Dishes, db: Session = Depends(get_db)):
+    dish = db.query(models.Dishes).filter(models.Dishes.id == target_dish_id).first()
+    if not dish:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'dish not found')
+    dish.title = request.title
+    dish.price = request.price
+    db.add(dish)
+    db.commit()
+    db.refresh(dish)
+    return dish
+
+
 @app.delete('/api/v1/menus/{target_menu_id}/submenus/{target_submenu_id}/dishes/{target_dish_id}',
-            status_code=status.HTTP_204_NO_CONTENT, tags=['Dishes'])
-def deleate_dish(dish_id, db: Session = Depends(get_db)):
-    dish = db.query(models.Dishes).filter(models.Dishes.id == dish_id)
-    if not dish.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    dish.delete()
+            status_code=status.HTTP_200_OK, tags=['Dishes'])
+def deleate_dish(target_dish_id, db: Session = Depends(get_db)):
+    db.query(models.Dishes).filter(models.Dishes.id == target_dish_id).delete(synchronize_session=False)
     db.commit()
